d6f9ad70afb7bf7619a2b92b2813d07558d86f3f
goudan <837471654@qq.com>
2022-04-12 05:12:45 -0400

    aio_read commit

--- asyncIO/aio.c
+++ asyncIO/aio.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define BUFF_SIZE 4096
 
         //     #include <aiocb.h>
         //     struct aiocb {
@@ -22,4 +23,34 @@
         //    };
 
         //    /* Operation codes for 'aio_lio_opcode': */
+
+int main(){
+    // prepare buffer
+    struct aiocb aio = {0};
+    int fd = open("test.txt", O_RDONLY);
+    if(-1 == fd) printf("can't open file %m");
+    aio.aio_buf = malloc(BUFF_SIZE + 1);
+    aio.aio_fildes = fd;
+    aio.aio_nbytes = BUFF_SIZE;
+    // operator of asyncIO : aio_read, aio_write, aio_error
+    int r = aio_read(&aio);
+    if(-1 == r){
+        printf("async read failed! %m\n");
+        close(fd);
+        free(aio.aio_buf);
+        exit(-1);
+    }
+    // check operator if finished
+    int n = 0;
+    while(aio_error(&aio)) n++;
+    // get the data of async
+    r = aio_return(&aio);
+    if(r>0){
+        printf("Get: n: %d, r: %d, data: %s", n, r, aio.aio_buf);
+    }
+    // the end
+    free(aio.aio_buf);
+    close(fd);
+    return 0;
+}
 
