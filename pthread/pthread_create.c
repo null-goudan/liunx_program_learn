1dcf0f48f9ea60f3f05863ec520563573c9f6dbe
goudan <837471654@qq.com>
2022-04-13 01:08:28 -0400

    pthread-arg-transfor

--- pthread/pthread_create.c
+++ pthread/pthread_create.c
@@ -5,6 +5,13 @@
 
 int num = 0;
 
+struct stu
+{
+    char name[32];
+    int score;
+};
+
+
 void pfuc(void *arg){
     while(1){
         printf("1-thread: num :%d\n", num++);
@@ -12,9 +19,22 @@ void pfuc(void *arg){
     }
 }
 
+void pfuc2(void *arg){
+    struct stu* data = (struct stu*)arg;
+    while(1){
+        printf("2-thread: num : %d\n data: name: %s, score: %d\n", num++, data->name, data->score);
+        sleep(1);
+    }
+}
+
 int main(){
-    pthread_t pid;
+    pthread_t pid, pid2;
+    struct stu stu1 = {
+        "yyx",
+        18
+    };
     pthread_create(&pid, NULL, pfuc, NULL);
+    pthread_create(&pid2, NULL, pfuc2, &stu1);
     while(1){
         printf("main-thread: num :%d\n", num++);
         sleep(1);
