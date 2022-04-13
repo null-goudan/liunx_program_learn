0586a00f90acec735b70c4d8188ed6051f5cdb46
goudan <837471654@qq.com>
2022-04-13 10:01:21 -0400

    pthread_spin-commit

--- pthread/pthread_spin.c
+++ pthread/pthread_spin.c
@@ -3,31 +3,31 @@
 #include <unistd.h>
 
 int n = 0;
-pthread_mutex_t mutelock;
+pthread_spin_t mutelock;
 void func1(void *arg){
     for(int i=0;i<5000000;i++){
-        pthread_mutex_lock(&mutelock);
+        pthread_spin_lock(&mutelock);
         n++;
-        pthread_mutex_unlock(&mutelock);
+        pthread_spin_unlock(&mutelock);
     }
 }
 
 void func2(void *arg){
     for(int i=0;i<5000000;i++){
-        pthread_mutex_lock(&mutelock);
+        pthread_spin_lock(&mutelock);
         n++;
-        pthread_mutex_unlock(&mutelock);
+        pthread_spin_unlock(&mutelock);
     }
 }
 
 int main(){
     pthread_t pid1, pid2;
-    pthread_mutex_init(&mutelock, NULL);
+    pthread_spin_init(&mutelock, NULL);
     pthread_create(&pid1, NULL, func1, NULL);
     pthread_create(&pid2, NULL, func2, NULL);
     pthread_join(pid1, NULL);
     pthread_join(pid2, NULL);
     printf("n:\t%d\n", n);
-    pthread_mutex_destroy(&mutelock);
+    pthread_spin_destroy(&mutelock);
     return 0;
 }
